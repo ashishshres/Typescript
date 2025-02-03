e05f5767ed4ad798ac2b3c9ff3d30df912e9910d
ashishshres <ashishshr27@gmail.com>
2025-02-03 10:19:00 +0545

    primitive types

---
+++ 02_Primitive_Types/app.ts
@@ -0,0 +1,13 @@
+/*
+Primitive types (number, string, boolean)
+*/
+
+let a = 12; // number
+let b = 12.34; // number
+// a = "ashish"; // Type 'string' is not assignable to type 'number'
+
+let myName = "ashish"; // string
+// myName = 2; // Type 'number' is not assignable to type 'string'
+
+let isActive = false; // boolean
+// isActive = 1; // Type 'number' is not assignable to type 'boolean'
