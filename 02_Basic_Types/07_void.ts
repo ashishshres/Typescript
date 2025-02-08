7d2f842ec02afb95b2930d72e2f48cd480073e6e
Ashish Shrestha <ashishshr27@gmail.com>
2025-02-08 10:35:55 +0545

    void

---
+++ 02_Basic_Types/07_void.ts
@@ -0,0 +1,13 @@
+/*
+Void
+In TypeScript, `void` is a type used to indicate that a function does not return a value. It is primarily used as the return type for functions that perform actions but do not return anything.
+*/
+
+// implicitly void
+// function greet() {
+//   console.log("Good morning");
+// }
+
+function greet(): void {
+  console.log("Good morning");
+}
