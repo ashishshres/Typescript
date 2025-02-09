94163722b28b392787fb5dc8725cd051330e4c30
Ashish Shrestha <ashishshr27@gmail.com>
2025-02-09 15:22:57 +0545

    null

---
+++ 02_Basic_Types/08_null.ts
@@ -0,0 +1,10 @@
+/*
+Null
+In TypeScript, `null` is a special type that represents 
+an intentional absence of value. It is one of JavaScript's 
+primitive values and behaves differently depending on 
+TypeScript's strictness settings.
+*/
+
+let emptyValue: null = null;
+// emptyValue = 20; // Type '20' is not assignable to type 'null'.
