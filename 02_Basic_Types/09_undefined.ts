928dc26c19b517d525980061067ee18b5f2e92b3
Ashish Shrestha <ashishshr27@gmail.com>
2025-02-09 15:23:02 +0545

    undefined

---
+++ 02_Basic_Types/09_undefined.ts
@@ -0,0 +1,11 @@
+/*
+Undefined 
+`undefined` represents a variable that has been declared but 
+not assigned a value.
+*/
+
+let notAssigned: undefined; //  allowed
+console.log(notAssigned); // undefined
+
+let declaredButNotAssigned: string;
+// console.log(declaredButNotAssigned); // variable 'declaredButNotAssigned' is used before being assigned.
